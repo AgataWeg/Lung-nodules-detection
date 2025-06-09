bb7d999b670eb6ecb83b9327a1150801e5bf1588
Agata Węglerska <agataweglik@gmail.com>
2025-06-08 18:59:48 -0700

    Slice thickness script calc

---
+++ data/slice_thickness.py
@@ -0,0 +1,27 @@
+import os
+import json
+import SimpleITK as sitk
+
+# configurations
+mhd_folder = "."
+output_json = "slice_thickness.json"
+
+mhd_files = [f for f in os.listdir(mhd_folder) if f.endswith(".mhd")]
+
+data = {"slice_thickness": []}
+
+for file in mhd_files:
+    file_id = os.path.splitext(file)[0]  # remove .mhd extension
+    ds = sitk.ReadImage(mhd_folder + "/" + file)
+    slice_thickness = ds.GetSpacing()[2]
+    entry = {
+        "slice_thickness": float(slice_thickness),
+        "image": file,
+    }
+    data["slice_thickness"].append(entry)
+
+# save to JSON
+with open(output_json, "w") as f:
+    json.dump(data, f, indent=4)
+
+print(f"JSON saved to {output_json}")
